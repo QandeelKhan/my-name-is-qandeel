f300aa30a1ceccce8d0a07f995a18b0481624583
Qandeel Khan <com.qandeelhaider@gmail.com>
2022-11-12 07:52:33 +0500

    updates...

--- src/components/HeroIntro.tsx
+++ src/components/HeroIntro.tsx
@@ -1,7 +1,7 @@
 // sfc stateles func component
 import qandeel from "../images/qandeel.jpeg";
 import df4 from "../images/df4.jpeg";
-import fs from "../images/fs.png";
+import df4close from "../images/df4-close.jpg";
 import Theme from "./theme";
 import * as Scroll from "react-scroll";
 import {
@@ -36,7 +36,7 @@ const HeroIntro = () => {
             </div>
             {/* left column */}
             <div className="left-column">
-                <img id="profile_pic" src={df4} alt="qandeel" />
+                <img id="profile_pic" src={df4close} alt="qandeel" />
                 <h5 style={{ textAlign: "center", lineHeight: 0 }}>
                     Personalize Theme
                 </h5>
@@ -55,8 +55,8 @@ const HeroIntro = () => {
                         <div id="corner-tr" className="corner"></div>
                         <h3>What I Do</h3>
                         <p>
-                            I was a lead developer and fast learner in past, now
-                            I enjoy my work as a fullstack developer.
+                            I was a lead developer and fast learner in past
+                            life, now I enjoy my work as a fullstack developer.
                         </p>
                         <div id="corner-br" className="corner"></div>
                         <div id="corner-bl" className="corner"></div>
